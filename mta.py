4327e3b1087587859deb81952ddb364ef3557185
georged <6376745+dodders@users.noreply.github.com>
2019-02-18 12:49:13 -0500

    working mta feed functions.

---
+++ mta.py
@@ -0,0 +1,45 @@
+from google.transit import gtfs_realtime_pb2
+import urllib.request
+import os
+from datetime import datetime
+
+
+def get_stop_times():
+    mta_url = os.getenv('MTAURL')
+    mta_key = os.getenv('MTAKEY')
+    mta_feedid = os.getenv('MTAFEEDID')
+    # bedford = 'L08N'
+    bedford = '115N'
+
+    feed = gtfs_realtime_pb2.FeedMessage()
+    # url = mta_url + mta_key + '&feed_id=' + mta_feedid
+    url = mta_url + mta_key + '&feed_id=' + '1'
+    response = urllib.request.urlopen(url)
+    feed.ParseFromString(response.read())
+
+    stop_times = []
+    for entity in feed.entity:
+        if hasattr(entity, 'trip_update'):
+            for stop_time_update in entity.trip_update.stop_time_update:
+                if stop_time_update.stop_id == bedford:
+                    try:
+                        dt = datetime.fromtimestamp(stop_time_update.arrival.time)
+                        stop_times.append(dt)
+                        if len(stop_times) >= 5:
+                            return stop_times
+                    except ValueError as err:
+                        print('invalid timestamp', stop_time_update.arrival.time, err)
+    return stop_times
+
+
+def get_next_trains():
+    print('starting get next trains...')
+    times = get_stop_times()
+    print('stop times:', times)
+    curr_time = datetime.now()
+    gaps = [round((t - curr_time).seconds / 60) for t in times]  # list of gaps in seconds...
+    print('gaps:', gaps)
+    return gaps
+
+
+print(get_next_trains())
